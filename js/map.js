da2712870b025d85e46ccd3cd46f1eb2ce9d6769
Maarten <post@mrutten.nl>
2019-02-14 20:59:36 +0100

    Change maxZoom.

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 var map = new L.Map('map', {center: new L.LatLng(0, 0), zoomControl: false});
 var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Esri',
-	maxZoom: 16,
+	maxZoom: 12,
 	minZoom: 3
 });
 var places = new L.KML("kml/places.kml", {async: true});
